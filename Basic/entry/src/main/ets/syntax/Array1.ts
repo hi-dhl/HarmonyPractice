0d4240dc6623fde65906d58f03e3032fd41abd5a
hi-dhl <hihuilong@gmail.com>
2024-01-21 23:58:09 +0800

    修改了注释

--- Basic/entry/src/main/ets/syntax/Array1.ts
+++ Basic/entry/src/main/ets/syntax/Array1.ts
@@ -1,9 +1,13 @@
 /**
- * 文件名以 ts 的结尾表示  TypeScript
- * 文件名以 ets 的结尾表示 ArkTS
- *
- * 基于 TypeScript 语法，下面代码只能在 TypeScript 中运行
- *  运行时需要导入 Person
+ * <pre>
+ *     author: 程序员DHL
+ *     date  : 2024/1/21
+ *     公众号 : ByteCode
+ *     desc  :
+ *            文件名以 ts 的结尾表示  TypeScript
+ *            文件名以 ets 的结尾表示 ArkTS
+ *            基于 TypeScript 语法，下面代码只能在 TypeScript 中运行
+ * </pre>
  */
 
 import {Student} from '../model/StudentTypeScript'
--- Basic/entry/src/main/ets/syntax/Constructor1.ts
+++ Basic/entry/src/main/ets/syntax/Constructor1.ts
@@ -1,10 +1,14 @@
 /**
- * 文件名以 ts 的结尾表示  TypeScript
- * 文件名以 ets 的结尾表示 ArkTS
- *
- * 基于 TypeScript 语法，下面代码只能在 TypeScript 中运行
+ * <pre>
+ *     author: 程序员DHL
+ *     date  : 2024/1/21
+ *     公众号 : ByteCode
+ *     desc  :
+ *            文件名以 ts 的结尾表示  TypeScript
+ *            文件名以 ets 的结尾表示 ArkTS
+ *            基于 TypeScript 语法，下面代码只能在 TypeScript 中运行
+ * </pre>
  */
-
 class Person {
   constructor(
     private name: string
